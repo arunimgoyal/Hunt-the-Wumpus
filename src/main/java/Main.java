04c5b54bac93468a11e71269152fcd8cd2e701f8
Arunim Goyal <4runimG1207@gmail.com>
2024-11-30 20:49:04 -0800

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,1 +1,14 @@
+public class Main {
+  
+  public static void main(String[] args){
+    
+    Trivia trivia = new Trivia();
+    //trivia.writeQuestion(trivia.addTriviaQuestion()); // For filewriting
 
+    Map mainMap = new Map("map");
+    Cave cave = new Cave(mainMap.getLocations());
+    new MainMenu(cave); // Start Game
+
+  }
+
+}
