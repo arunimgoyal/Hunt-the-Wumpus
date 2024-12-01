762adc46cc3c81996981a8492b0ef1d95882fa43
Arunim Goyal <4runimG1207@gmail.com>
2024-11-30 20:42:56 -0800

    Update Hazard.java

--- src/main/java/Hazard.java
+++ src/main/java/Hazard.java
@@ -1,1 +1,48 @@
+public class Hazard{
+  // PROPERTIES 
+  private Location loc;
+  private String type;
 
+  // CONSTRUCTOR 
+  public Hazard(String type){
+    this.type = type;
+  }
+
+  // METHODS 
+  public void wumpusAction(Cave cave){
+    this.loc.setHazard(new Hazard("none")); // Set current Loc to a hazard with no value
+    this.loc = cave.getRandomEmptyLocation(); // Set this hazards location = to the rand
+    this.loc.setHazard(this); // Set the locations hazard into this
+    System.out.println("Wumpus Location: " + loc);
+
+  }
+
+  public Location batsAction(Cave cave){
+    //this.loc = cave.getRandomLocation(); // Set a new Loc
+    this.loc.setHazard(new Hazard("none")); // Set current Loc to a hazard with no value
+    this.loc = cave.getRandomEmptyLocation();; // Set this hazards location = to the rand
+    this.loc.setHazard(this); // Set the locations hazard into this
+    System.out.println("Bats Location: " + loc);
+    return cave.getRandomEmptyLocation(); // Return a new Random Player Location
+  }
+
+  public Location pitsAction(Location prev){
+    return prev;
+  }
+
+  public Location getLoc(){
+    return loc;
+  }
+
+  public void setLoc(Location loc){
+    this.loc = loc;
+  }
+
+  public String getType(){
+    return type;
+  }
+
+  public void setType(String newType){
+    this.type = newType;
+  }
+}
